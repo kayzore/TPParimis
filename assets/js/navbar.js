b034bf15f6b3fc702bcbb1f8ef78ae8361a4d8a8
Kayzore <jerome.lelievre2@gmail.com>
2017-01-12 21:45:56 +0100

    Add to navbar.js detection onload page scrollToTop Change menu navigation dropdown restaurant to link Edit error text in chambre-suite.html

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+    if($(window).scrollTop() > 0) {
+        $('nav').addClass('goToTop');
+    }
     $(window).bind('scroll', function () {
         var navHeight = 300; // custom nav height
         ($(window).scrollTop() > navHeight) ? $('nav').addClass('goToTop'): $('nav').removeClass('goToTop');
