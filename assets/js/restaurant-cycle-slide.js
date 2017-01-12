71d48b4a9856e67583978b922f2374e9ae5d7d11
Kayzore <jerome.lelievre2@gmail.com>
2017-01-12 15:25:57 +0100

    Add restaurant parts

---
+++ assets/js/restaurant-cycle-slide.js
@@ -0,0 +1,9 @@
+/*global $*/
+$(document).ready(function () {
+    "use strict";
+    $('#listImageRestaurant').cycle({
+        fx: 'scrollLeft',
+        sync: true,
+        delay: -3000
+    });
+});
