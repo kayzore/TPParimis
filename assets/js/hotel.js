d84f892d6cae2e2ccacf32ea83aa1b82d99b211c
Kayzore <jerome.lelievre2@gmail.com>
2017-01-13 12:24:21 +0100

    Add content to hotel page Edit css for this

---
+++ assets/js/hotel.js
@@ -0,0 +1,13 @@
+/*global $*/
+$(document).ready(function () {
+    "use strict";
+    $('#mag1').click(function () {
+        $('#magModal1').modal('show');
+    });
+    $('#mag2').click(function () {
+        $('#magModal2').modal('show');
+    });
+    $('#mag3').click(function () {
+        $('#magModal3').modal('show');
+    });
+});
