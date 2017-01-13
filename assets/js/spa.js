ca1d74bf965741f5a4bbb5615b78db3923e9e866
Kayzore <jerome.lelievre2@gmail.com>
2017-01-13 11:01:19 +0100

    Delete useless img Add lang fr on all page Add img for spa Create spa page Edit css for spa page

---
+++ assets/js/spa.js
@@ -0,0 +1,63 @@
+/*global $*/
+$(document).ready(function () {
+    "use strict";
+    var removeActive;
+
+    if ($(window).scrollTop() > 0) {
+        $('aside>div').addClass('goAffixTop');
+    }
+
+    $(window).bind('scroll', function () {
+        var affixHeight = 300; // custom nav height
+        ($(window).scrollTop() > affixHeight) ? $('aside>div').addClass('goAffixTop'): $('aside>div').removeClass('goAffixTop');
+    });
+
+    $('a#btnSoinsDetente').click(function (evt) {
+        evt.preventDefault();
+        removeActive();
+        $(this.parentNode).addClass('active');
+        $('html, body').animate({
+            scrollTop: $('#detente').offset().top - 175
+        }, 2000);
+    });
+
+    $('a#btnSoinsDecouverte').click(function (evt) {
+        evt.preventDefault();
+        removeActive();
+        $(this.parentNode).addClass('active');
+        $('html, body').animate({
+            scrollTop: $('#decouverte').offset().top - 175
+        }, 2000);
+    });
+
+    $('a#btnSoinsRelaxant').click(function (evt) {
+        evt.preventDefault();
+        removeActive();
+        $(this.parentNode).addClass('active');
+        $('html, body').animate({
+            scrollTop: $('#relaxant').offset().top - 175
+        }, 2000);
+    });
+
+    $('a#btnSoinsPlaisir').click(function (evt) {
+        evt.preventDefault();
+        removeActive();
+        $(this.parentNode).addClass('active');
+        $('html, body').animate({
+            scrollTop: $('#plaisir').offset().top - 175
+        }, 2000);
+    });
+
+    $('a#btnSoinsEvasion').click(function (evt) {
+        evt.preventDefault();
+        removeActive();
+        $(this.parentNode).addClass('active');
+        $('html, body').animate({
+            scrollTop: $('#evasion').offset().top - 175
+        }, 2000);
+    });
+
+    removeActive = function removeActive() {
+        $('aside .active').removeClass('active');
+    };
+});
