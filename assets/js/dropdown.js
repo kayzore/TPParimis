5e791c8b32b58226cb34aa0f0599275194f877b0
Kayzore <jerome.lelievre2@gmail.com>
2017-01-15 16:55:25 +0100

    Ajout d'animation pour les dropdown bootstrap

---
+++ assets/js/dropdown.js
@@ -0,0 +1,12 @@
+/*global $*/
+$(document).ready(function () {
+    "use strict";
+    
+    $('.dropdown').on('show.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideDown();
+    });
+
+    $('.dropdown').on('hide.bs.dropdown', function () {
+        $(this).find('.dropdown-menu').first().stop(true, true).slideUp();
+    });
+});
