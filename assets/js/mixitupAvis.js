dae3fc1faba6925f73b2bbd45de576a1e87437e3
Kayzore <jerome.lelievre2@gmail.com>
2017-01-16 12:41:05 +0100

    Ajout de partage dans le footer Ajout d'un lien vers mon site dans le footer Creation d'un trie des commentaire sur la page avis Rectification d'oubli de champs dans hotel/recrutement et contact

---
+++ assets/js/mixitupAvis.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    var container = document.querySelector('.listAvis'),
+        mixer = mixitup(container, {
+            controls: {
+                scope: 'local'
+            }
+        });
+});
