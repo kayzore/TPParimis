bff317bc54ff44be0165a049bfe5f8b6edc29a1f
Kayzore <jerome.lelievre2@gmail.com>
2017-01-16 23:36:41 +0100

    Ajout d'un slide sur le coté de la page d'accueil pour afficher la zone de recherche et de langue

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -8,15 +8,26 @@ $(document).ready(function () {
 
     $(window).bind('scroll', function () {
         var navHeight = 250; // Hauteur de déclenchement du position fixed
-
         if ($(window).scrollTop() > navHeight) {
+            $('#other').css({top: 60});
             $('nav').addClass('goToTop');
             $('#btnGoTop').fadeIn('slow');
         } else {
+            $('#other').css({top: 50, backgroundColor: "darkgram"});
             $('nav').removeClass('goToTop');
             $('#btnGoTop').fadeOut('slow');
         }
-
+    });
+    
+    
+    $('#other').mouseenter(function () {
+        $(this).animate({opacity: 0.8, right: '0'}, 500);
+        $('#other #arrowRight').addClass('fa-rotate-180');
+    });
+    
+    $('#other').mouseleave(function () {
+        $(this).animate({opacity: 0.5, right: '-310px'}, 500);
+        $('#other #arrowRight').removeClass('fa-rotate-180');
     });
 
     // Animation pour le retour en haut de page
